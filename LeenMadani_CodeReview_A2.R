303738afe0ff315bcfb758c53f05d2d0e7e59e6c
SoumyaShastri <135373668+SoumyaShastri@users.noreply.github.com>
2023-06-16 19:09:08 -0400

    Finalized comments for Code review

--- LeenMadani_CodeReview_A2.R
+++ LeenMadani_CodeReview_A2.R
@@ -34,6 +34,27 @@ if (!is.na(as.numeric(answer))) { # checks if input is a valid numeric value and
 
 #*Code Review: Soumya Shastri 
 
+#*Line 5: Prompt runs well. I would suggest a more specific prompt that tells 
+#*the user exactly what you want, in our case we want the user to specifically 
+#*enter a three-digit positive number. By saying "enter a number", there is a 
+#*greater chance for them to run into an error message and not knowing the reason 
+#*behind it. 
+#*Line 7-18: Great explanation! Another method to extract digits is using the 
+#*strsplit function but what you have works just as well
+#*Line 21-22: these functions ensure that the entry is numeric and positive 
+#*however, an additional code is required in the outer if statement to ensure the
+#*prompt is only three digits since it is also part of the validation task. This can be 
+#*done by using the following (nchar(answer) == 3) for example. 
+#*I see the range limits you created in lines 24-25 however since it's part of
+#*the inner if statement, entering a 4-digit narcissistic number such as 1634
+#*will result in a program output saying "is NOT a narcissistic Armstrong number" 
+#*when it should actually say "Entry is not valid, Terminating..." 
+#*The rest of the program functions as intended, and it is evident why you have 
+#*taken particular actions, as indicated by your detailed comments. For upcoming 
+#*assignments, I would suggest providing more precise prompts to enable users to 
+#*effectively utilize your program. Additionally, exercise caution when dealing 
+#*with nested if functions.  Well done, it was a pleasure to review your code :) 
+
 
 
 
